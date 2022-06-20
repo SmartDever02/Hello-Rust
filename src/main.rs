bb510b5c17015c7e3dbd0cc718aa153890322326
SmartDever02 <smart.jamesjin@gmail.com>
2022-06-19 20:12:18 -0400

    struct & its method

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
-use std::io;
+// use std::io;
+
+// fn main() {
+//     let a = [1, 2, 3, 4, 5];
+
+//     println!("Please enter an array index.");
+
+//     let mut index = String::new();
+
+//     io::stdin()
+//         .read_line(&mut index)
+//         .expect("Failed to read line");
+
+//     let index: usize = index
+//         .trim()
+//         .parse()
+//         .expect("Index entered was not a number");
+
+//     let element = a[index];
+
+//     println!(
+//         "The value of the element at index {} is: {}",
+//         index, element
+//     );
+// }#[derive(Debug)]
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-
-    println!("Please enter an array index.");
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
-        "The value of the element at index {} is: {}",
-        index, element
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
     );
 }
